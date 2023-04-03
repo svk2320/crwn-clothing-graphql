f8c929c424c35cf53307ab8162b328482356a57c
username <vasanthkumars2025@gmail.com>
2023-04-03 11:17:20 +0530

    added graphQL

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,42 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+// import gql from 'graphql-tag';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  console.log(data);
 
-    getCategoriesMap();
-  }, []);
+  // useEffect(() => {
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     setCategoriesMap(categoryMap);
+  //   };
+
+  //   getCategoriesMap();
+  // }, []);
 
   const value = { categoriesMap };
   return (
